f74cd1814e460455ea3a8370d70ab6fb86182814
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-06 14:29:25 +0000

    initial

--- Practise.java
+++ Practise.java
@@ -1,9 +1,10 @@
-/**
- * Practise
- */
-public class Practise {
-    public static void main(String[] args) {
-        System.out.println("allah");
+ class Practise     // Here is the class name
+ {
+    public static void main(String[] args)    // This is the main methods And execution start here 
+    {
+        
+
+        System.out.println("allah");  // to print any things use this print statement
     }
 
     
