4d6fca0e8f28f3f307ea76bbdaf265b93d50ae1f
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-08 17:57:58 +0000

    checking num

---
+++ LargeNum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// This Program to Check Greater Number among three Number
+
+
+public class LargeNum {
+     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);    // Scanner Pre defined used to take input by user
+        System.out.println("Enter the three numbers");   // print  statement
+        int a = s.nextInt();    // pass the refrence variable
+        int b = s.nextInt();
+        int c = s.nextInt();
+        int max = checkLarge(a,b,c);
+        System.out.println("large Number is  "+max);
+
+
+    }
+    static int checkLarge(int a ,int b,int c){
+        int max = a>b && a>c?a:(b>c?b:c);
+
+        return max;
+    }
+
+    
+}
