8326e596f99e1cc058ea650a5223021e8f4efa9d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-13 17:24:19 +0000

    global Variable

---
+++ Rose.java
@@ -0,0 +1,12 @@
+class Rose{
+
+    int price = 50;
+
+    String color = "Red";
+    public static void main(String[] args) {
+        
+
+        Rose R1 = new Rose();
+        Rose R2 = new Rose();
+    }
+}
