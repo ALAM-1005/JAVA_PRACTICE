30b296631af514f21e5661d66fb25a6031758425
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-14 01:33:20 +0000

    Custom Constructer

---
+++ Mobile1.java
@@ -0,0 +1,18 @@
+public class Mobile1 {
+
+    private Mobile1(){
+
+
+        System.out.println("Custome Constructor");
+    }
+
+    public static void main(String[] args) {
+        
+
+        Mobile1 M = new Mobile1();
+
+
+        System.out.println(M);
+    }
+    
+}
