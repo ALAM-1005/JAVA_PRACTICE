defc19ffd1c17193499d53fbcdc5351c3bf7be62
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-14 18:23:13 +0000

    initial

--- DayName.java
+++ DayName.java
@@ -1,7 +1,8 @@
-/**
- * DayName
- */
-public class DayName {
+import java.util.Scanner;
+class DayName {
 
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter the  NO  Day ");
     
 }
