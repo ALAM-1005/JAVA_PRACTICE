09ac53b573f9c3f2128a11663e83e54616936534
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-14 01:39:36 +0000

    initial

--- Mobile1.java
+++ Mobile1.java
@@ -1,9 +1,18 @@
 public class Mobile1 {
 
+    int price;
+    String name,brand;
+
     private Mobile1(){
 
+        this.name = name;
+        this.brand = "Sumsang";
+        this.price = 124563;
 
-        System.out.println("Custome Constructor");
+
+
+
+       // System.out.println("Custome Constructor");
     }
 
     public static void main(String[] args) {
