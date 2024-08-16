da1099a98d7382d806b07db39c4bc05453bd948d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-16 16:39:12 +0000

    dayName

--- DayName.java
+++ DayName.java
@@ -1,8 +1,62 @@
 import java.util.Scanner;
-class DayName {
+class DayNum {
 
-    Scanner sc = new Scanner(System.in);
+    public static  void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Number of Days");
+
+        int Day = sc.nextInt();
+
+        weekDay(Day);
+    }
     
-    System.out.println("Enter the  NO  Day ");
-    
+    static void weekDay(int num){
+
+        switch (num) {
+            case 1:
+
+            System.out.println("Sunday");
+                
+                break;
+
+            case 2:
+
+            System.out.println("Monday");
+
+                break;
+
+            case 3:
+
+            System.out.println("Tuesday");
+
+                break;
+            case 4:
+
+            System.out.println("wednesday");
+
+                break;
+            case 5:
+
+            System.out.println("Thruesday");
+
+                break;
+            case 6:
+
+            System.out.println("Friday");
+
+                break;
+            case 7:
+
+            System.out.println("Saturday");
+
+                break;
+
+        
+            default:
+                System.out.println("Invalid data ");
+                break;
+        }
+    }
+
 }
---
+++ Digit.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Degine a method to display each digit in the given Number.
+ */
+public class Digit {
+
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter the number");
+
+        int n = input.nextInt();
+
+        while (n!=0) {
+
+            int rem = n%10;
+            System.out.println(rem);
+
+            n = n/10;
+            
+        }
+     }
+}
