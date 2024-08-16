2a4ee93e5193ab1aaea03bee0589906ed1692bb1
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-16 16:42:24 +0000

    blocks

--- Rose.java
+++ Rose.java
@@ -3,13 +3,14 @@ class Rose{
     int price = 50;   // global Variable
 
     String color = "Red";   // Global Variable
-    public static void main(String[] args) {
+    public static void main(String[] args) // main maethod  here so execution starts here
+    {
         
 
         Rose R1 = new Rose();  // Object Creation
         Rose R2 = new Rose();  // Object Creation
 
-        
+        System.out.println(R1.color);  // inheritate the global variable using object creatioon
 
 
     }
