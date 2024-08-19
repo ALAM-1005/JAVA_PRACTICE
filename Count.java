57f66de5ea75de8fb88895df334e22bc5acc847b
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-19 16:10:08 +0000

    intial

---
+++ Count.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Count {
+    
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter the Number ");
+
+        
+        
+        int n = sc.nextInt();
+
+        CountNo(n);
+
+
+    }
+    static void CountNo(int n){
+
+        int Count = 0;
+        do{
+            Count++;
+            n/=10;
+        }while(n!=0);
+
+        System.out.println("Count  Of Digit is :"+Count);
+    }
+
+    
+}
