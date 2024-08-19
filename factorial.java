e0da16c77bdf14a84851c3254882357308b0f9b5
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-19 16:30:41 +0000

    initial

---
+++ factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter the Number ");
+
+        
+        
+        int n = sc.nextInt();
+
+        factorial(n);
+        System.out.println(factorial(n));
+
+
+    }
+    static int factorial(int n){
+
+        int fact = n;
+
+        for(int i=2;i<n;i++){
+
+            fact *= i;
+        }
+        return fact;
+    }
+}
