24c3196088528882ecfe250463e72548e21b22d9
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-19 17:25:58 +0000

    initial

--- DigitSumation.java
+++ DigitSumation.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class DigitSumation {
+    public static void main(String[] args) {
+        
+
+        Scanner sc =new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+        int flSum = n%10;
+        int midSum = 0;
+        n = n/10;
+
+        while(n/10 != 0){
+
+            int rem = n%10;
+
+            midSum += rem;
+
+            n/=10;
+        }
+
+        flSum = flSum+n;
+    }
     
 }
