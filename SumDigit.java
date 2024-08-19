3ce8ee316c39b1c787ac53eef3581c8f60b4a52d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-19 16:14:56 +0000

    initial

---
+++ SumDigit.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SumDigit {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter the Number ");
+
+        
+        
+        int n = sc.nextInt();
+
+        Digit(n);
+
+
+    }
+
+    static int Digit(int n){
+
+        int sum = 0;
+
+        do{
+
+            sum +=(n%10);
+            n = n/10;
+
+        }while(n!=0);
+
+        return sum;
+    }
+    
+}
