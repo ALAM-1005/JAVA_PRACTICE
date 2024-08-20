fab132d6f08ac9bb31c536ac0999fbfe0694632b
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-20 17:56:18 +0000

    initial

--- Fact.java
+++ Fact.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Fact {
 
-    
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        sum  = 0;
+        System.out.println("Enter the Number ");
+
+        int n = sc.nextInt();
+
+        fact(n);
+
+        System.out.println();
+     }
+
+     static void fact(int n){
+
+        
+        for(int i= 1;i<=n;i++){
+
+            
+
+            if (n%i == 0) {
+
+                System.out.println(i);
+
+                sum += i;
+                
+            }
+        }
+
+     }
     
 }
