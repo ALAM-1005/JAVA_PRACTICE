59db6fcce34df6b0a85644c6d121d6b52e94345f
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-20 14:16:00 +0000

    initial

--- Even.java
+++ Even.java
@@ -1,3 +1,31 @@
-public class Even {
-    
+/**
+ * Define a method to display Even Number b/w the limits(user Enter Number )
+ * 
+ */
+
+ import java.util.Scanner;
+
+class Even {
+
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" enter the number");
+
+        int a = sc.nextInt() ;
+
+        int b = sc.nextInt();
+        System.out.println("-------------");
+
+        // for(int i=a;i<=b;i++)
+        for(;a<=b;a++)         
+        {
+
+            if(a%2 == 0)
+            System.out.println(a+" Even");
+        }
+
+    }
 }
