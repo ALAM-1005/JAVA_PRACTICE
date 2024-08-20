4bb7112624a027e0260c3ec6fd07763ea1909f15
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-20 14:19:37 +0000

    initial

---
+++ Odd.java
@@ -0,0 +1,31 @@
+/**
+ * Define a method to display Even Number b/w the limits(user Enter Number )
+ * 
+ */
+
+ import java.util.Scanner;
+
+class Odd {
+
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" enter the number");
+
+        int a = sc.nextInt() ;
+
+        int b = sc.nextInt();
+        System.out.println("-------------");
+
+        // for(int i=a;i<=b;i++)
+        for(;a<=b;a++)         
+        {
+
+            if(a%2 == 1)
+            System.out.println(a+" ODD ");
+        }
+
+    }
+}
