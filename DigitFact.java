a4e71cf8996ce21a9b7c3e85eb260fd16c628131
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-21 03:15:13 +0000

    initial

--- DigitFact.java
+++ DigitFact.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class DigitFact {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+      
+        System.out.println("Enter the Number ");
+
+        int n = sc.nextInt();
+
+        accessDigit(n);
+
+        System.out.println();
+     }
+
+     static void accessDigit(int n){
+
+        do{
+
+            int rem = n%10;
+            fact(rem);
+            n/=10;
+
+
+        }while(n!=0);
+     }
+
+     static void fact(int n){
+        int fact = n;
+        for(int i =2;i<n;i++){
+            fact *= i;
+        }
+
+        System.out.println("fact of "+n+" is "+fact);
+     }
     
 }
