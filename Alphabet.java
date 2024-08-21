215cbb657241fce437834d55549009b9cc24850f
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-21 17:34:41 +0000

    initial

--- Alphabet.java
+++ Alphabet.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Alphabet {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        displayAlphabet(n);
+       
+    }
+    public static void displayAlphabet(){
+
+        for(char i = 'A';i<= 'Z';i++){
+
+            System.out.println(i+ " ");
+
+        }
+    }
     
 }
