2890f5408454f82543aefb4b3c9754d26cca8ece
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-21 17:40:53 +0000

    initial

--- Alphabet.java
+++ Alphabet.java
@@ -15,11 +15,16 @@ public class Alphabet {
     }
     public static void displayAlphabet(char ch){
 
-        for(char i = 'A';i<= 'Z';i++){
+        for(char i = 'A';i<='Z';i++){
 
             System.out.println(i + " ");
 
         }
+        System.out.println();
     }
+
+        for(char i = 'a';i<='z';i++){
+            System.out.println(i + " ");
+        }
     
 }
