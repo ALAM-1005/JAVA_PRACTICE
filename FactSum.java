e3bb396aa17625df594f418565724356f679d207
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-21 17:22:10 +0000

    initial

--- FactSum.java
+++ FactSum.java
@@ -1,3 +1,41 @@
+
+ /**
+ * Define a method to find sumation of  Factors of given Number.
+ */
+
+import java.util.Scanner;
+
 public class FactSum {
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+      
+        System.out.println("Enter the Number ");
+
+        int n = sc.nextInt();
+
+        fact(n);
+
+        System.out.println();
+     }
+
+     static void fact(int n){
+        
+        for(int i= 1;i<=n;i++){
+            int Sum = 0;
+            
+            if (n%i == 0) {
+
+                
+
+                Sum += i;
+            }
+            System.out.println(Sum);
+        } 
+
+     }
     
 }
+
+
