433e6e49ba83788a0a776826f5df8a9f4e23284d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-21 17:19:10 +0000

    initial

--- Fact.java
+++ Fact.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 public class Fact {
 
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+        Scanner scan =  new Scanner(System.in);
 
       
         System.out.println("Enter the Number ");
 
-        int n = sc.nextInt();
+        int n = scan.nextInt();
 
         fact(n);
 
