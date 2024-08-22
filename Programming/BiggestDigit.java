a44492a20d977764b6a9b34c5bde82a3fc629dea
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-22 03:42:04 +0000

    initial

--- Programming/BiggestDigit.java
+++ Programming/BiggestDigit.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class BiggestDigit {
 
+    // static method starts here
     public static int getMaxDigit(int number) {
-        int maxDigit = 0;
+        int maxDigit = 0;  // Assume the maximum number is zero.
         while (number > 0) {
-            int digit = number % 10;
+            int digit = number % 10;  // here we get the remonder means  last digit 
             if (digit > maxDigit) {
                 maxDigit = digit;
             }
             number /= 10;
         }
-        return maxDigit;
+        return maxDigit;  // return statement 
     }
     
 
-
+// main method starts here 
     public static void main(String[] args) {
     
         Scanner sc = new Scanner(System.in);
