1b77aaf14fe8133d8a8f2b44083aa6e1a440cfbb
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-22 03:35:11 +0000

    intial

--- Programming/BiggestDigit.java
+++ Programming/BiggestDigit.java
@@ -1,5 +1,27 @@
 package Programming;
 
+import java.util.Scanner;
+
 public class BiggestDigit {
+
+    public static int getMaxDigit(int number) {
+        int maxDigit = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit > maxDigit) {
+                maxDigit = digit;
+            }
+            number /= 10;
+        }
+        return maxDigit;
+    }
     
+
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+}
+    
+
 }
