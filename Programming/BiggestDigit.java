4549ea839a8c2fdb32078d07ffe18dda18ac56f8
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-22 03:36:36 +0000

    initial

--- Programming/BiggestDigit.java
+++ Programming/BiggestDigit.java
@@ -21,6 +21,10 @@ public class BiggestDigit {
     public static void main(String[] args) {
     
         Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        System.out.println(getMaxDigit(number));
 }
     
 
