021b6edeb019aa10d737ab26bd452f48d62482e7
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-25 11:23:59 +0000

    initial

--- Programming/ArmstrongNum.java
+++ Programming/ArmstrongNum.java
@@ -42,16 +42,18 @@ public class ArmstrongNum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter the Number");
+
         int n = scan.nextInt();
 
         boolean rs = isAmstrongNum(n);
 
         if(rs){
-            System.out.println(n+"is a Amstrong Number");
+            System.out.println(n+"   is a Amstrong Number");
         }
 
         else{
-            System.out.println(n+"is not a ArmstrongNumber");
+            System.out.println(n+"  is not a ArmstrongNumber");
         }
 
     }
