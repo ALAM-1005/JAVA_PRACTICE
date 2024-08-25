42e161ce04c1fa71282bc3b7f9fd49f057f92c14
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-25 11:20:30 +0000

    initial

--- Programming/ArmstrongNum.java
+++ Programming/ArmstrongNum.java
@@ -6,13 +6,45 @@ public class ArmstrongNum {
 
     static int getPow(int n,int p){
 
+        int prod = 1;
+        while (p>0) {
+
+             prod = prod*n;
+            p--;
+        }
+        return prod;
+
+    }
+
+    static int getNoOfDigits(int n){
+        int count = 0;
+
+        do{
+            count ++;
+            n/=10;
+        }while(n!=0);
+        return count;
+    }
+
+    
+
+    static boolean isAmstrongNum(int n){
+        int sum= 0,temp =n;
+        int count =  getNoOfDigits(n);
+        do{
+
+            int d = n%10;
+            sum= sum+getPow(d, count);
+            n/=10;
+        }while(n!=0);
+        return sum == temp;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
 
-        int rs = isAmstrongNum(n);
+        boolean rs = isAmstrongNum(n);
 
         if(rs){
             System.out.println(n+"is a Amstrong Number");
