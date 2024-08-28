bb0f25103c6fc8f6726fa773461e7f649052a1a3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-28 18:23:35 +0000

    initial

--- NumberSystem/Happy.java
+++ NumberSystem/Happy.java
@@ -11,37 +11,23 @@ public class Happy {
             n = sumOfSquareOfDigits(n);
             
         }
-
         return n==1 || n==7;
     }
-
     static int sumOfSquareOfDigits(int x){
-
-        int sum = 0;
-
+       int sum = 0;
         while (x!=0) {
             int d = x%10;
             sum = sum+d*d;
-
-            x= x/10;
-            
+            x= x/10;           
         }
-
         return sum;
     }
 
     public static void main(String[] args) {
-        
-        
-        
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter the Number");
-
         int n = sc.nextInt();
-
         boolean rs = isHappy(n);
-
         if (rs) {
             System.out.println(n+"is a Happy Number");
         }
