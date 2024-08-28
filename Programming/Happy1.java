27bb2623490d279e82a409cf26cbbb237009b645
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-28 18:51:46 +0000

    initial

--- Programming/Happy1.java
+++ Programming/Happy1.java
@@ -1,5 +1,39 @@
 package Programming;
+public class HappyNum {
 
-public class Happy1 {
+    static boolean isHappy(int n){
+
+        while (n>9) {
+
+            n = sumOfSquareOfDigits(n);
+            
+        }
+        return n==1 || n==7;
+    }
+    static int sumOfSquareOfDigits(int x){
+       int sum = 0;
+        while (x!=0) {
+            int d = x%10;
+            sum = sum+d*d;
+            x= x/10;           
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+    
+
+
+        for(int i = 1; i<=100;i++){
+    
+            boolean rs = isHappy(i);
+    
+            if(rs)
+                System.out.print(i+" ");
+    
+      }
+
+    }
+    
     
 }
