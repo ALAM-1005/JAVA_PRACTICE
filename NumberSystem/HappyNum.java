aa9c29d508d4ec21d81c330890ad5a969ba89fd2
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-28 17:51:11 +0000

    initial

--- NumberSystem/HappyNum.java
+++ NumberSystem/HappyNum.java
@@ -1,5 +1,50 @@
 package NumberSystem;
 
+import java.util.Scanner;
+
 public class HappyNum {
+
+    static boolean isHappy(int n){
+
+        while(n>9){
+
+            int sum= 0;
+
+            do{
+                int d = n%10;
+
+                sum  = sum+d*d;
+
+            }while(n!=0);
+
+            n=sum;
+        }
+
+        return n == 1 || n== 7;
+    }
+
+
+    public static void main(String[] args) {
+        
+        
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Number");
+
+        int n = sc.nextInt();
+
+        boolean rs = isHappy(n);
+
+        if (rs) {
+            System.out.println(n+"is a Happy Number");
+        }
+        else{
+            System.out.println(n+"is not a happy Number");
+        }
+    }
+
+
+
     
 }
