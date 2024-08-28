717cd49a2eef760d4db7d959d2d764e094ae15a0
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-28 02:57:41 +0000

    initial

--- NumberSystem/HarshadNum.java
+++ NumberSystem/HarshadNum.java
@@ -2,8 +2,36 @@ package NumberSystem;
 
 public class HarshadNum {
 
-    public static void main(String[] args) {
-        System.out.println("Allah");
+
+    static int sumOfDigit(int n){
+
+        int sum = 0;
+
+        do{
+
+            sum = sum+n%10;
+            n/=10;
+
+        }while(n!=0);
+
+        return sum;
+    }
+
+
+    static boolean isHarshad(int n){
+
+        return n%sumOfDigit(n)  == 0;
     }
     
+    public static void main(String[] args) {
+        for(int i = 1; i<=100;i++){
+    
+            boolean rs = isHarshad(i);
+    
+            if(rs)
+                System.out.print(i+" ");
+    
+          
+        }
+      }
 }
