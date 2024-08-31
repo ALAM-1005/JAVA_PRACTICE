3dbcfedb31ee06e3a6b97a346c09dbab92579528
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-31 17:29:06 +0000

    initial

--- Star.java
+++ Star.java
@@ -5,6 +5,8 @@ class Star{
 public static void main(String[] args) {
     Scanner sc  = new Scanner (System.in);
 
+    System.out.println("Enter the number");
+
     int n = sc.nextInt();
 
     for(int i = 1;i<=n;i++){
