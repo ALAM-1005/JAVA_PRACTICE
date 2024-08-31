ed5a4c43d6445b66a9b140761425f56c9a607db0
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-31 17:37:43 +0000

    initial

--- Pattern/Star1.java
+++ Pattern/Star1.java
@@ -13,9 +13,11 @@ public static void main(String[] args) {
     int n = sc.nextInt();
 
     for(int i=1;i<=n;i++){
-        
+
         System.out.println("*");
     }
+
+    sc.close();
 }
     
 }
