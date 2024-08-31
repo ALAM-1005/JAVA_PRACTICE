e5e688c6524e11122d9ada6777fd84d15c3c16f1
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-08-31 17:30:18 +0000

    initial

---
+++ Pattern/Star.java
@@ -0,0 +1,20 @@
+package Pattern;
+import java.util.Scanner;
+
+class Star{
+
+public static void main(String[] args) {
+    Scanner sc  = new Scanner (System.in);
+
+    System.out.println("Enter the number");
+
+    int n = sc.nextInt();
+
+    for(int i = 1;i<=n;i++){
+
+
+        System.out.println("*");
+    }
+}
+    
+}
