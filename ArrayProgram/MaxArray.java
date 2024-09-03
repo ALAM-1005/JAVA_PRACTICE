3cfb40549dd8650a606cb2c3457f804316c4668c
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 18:40:50 +0000

    max arrays

---
+++ ArrayProgram/MaxArray.java
@@ -0,0 +1,55 @@
+package ArrayProgram;
+
+import java.util.Scanner;
+
+public class MaxArray {
+    static int getSmallest(int[] x){
+
+        int small = Integer.MIN_VALUE;
+
+        for(int i = 0;i<x.length;i++){
+
+            if ((x[i]<small)) {
+                
+                small = x[i];
+            }
+
+            
+        }
+        return small;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of an Arrays  : ");
+
+        int n = sc.nextInt();
+
+        int[] ar = new int[n];
+
+        System.out.println ("Enter"+n+"Values");
+
+        for(int i =0;i<ar.length;i++){
+
+            ar[i]  = sc.nextInt();
+        }
+
+        System.out.println("User entered Array Elements");
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.print(ar[i]+"  ");
+        }
+        System.out.println();
+
+        System.out.println("-----------------------------------------------");
+
+        int sm = getSmallest(ar);
+
+        System.out.println("Smallest element in array is :"+sm);
+
+    }
+    
+}
