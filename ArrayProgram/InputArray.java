7befe7b1ab0ccd13fdece52f85f8ca7d2e2eae7a
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 18:09:13 +0000

    initial

--- ArrayProgram/InputArray.java
+++ ArrayProgram/InputArray.java
@@ -1,7 +1,29 @@
 package ArrayProgram;
 
+import java.util.Scanner;
+
 public class InputArray {
 
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of an Arrays  : ");
+
+        int n = sc.nextInt();
+
+        int[] ar = new int[n];
+        System.out.println("Enter"+n+"Values");
+
+        for(int i =0;i<ar.length;i++){
+            ar[i]  = sc.nextInt();
+        }
+
+        System.out.println("User entered Array Elements");
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.println(ar[i]+" ");
+        }
+    }
     
 }
