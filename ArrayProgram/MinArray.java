acdec7ea88c8d07452c69436a4f90c69ecb3180d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 18:25:52 +0000

    minimum elements in arrays

--- ArrayProgram/MinArray.java
+++ ArrayProgram/MinArray.java
@@ -1,11 +1,13 @@
 package ArrayProgram;
 
+import java.util.Scanner;
+
 public class MinArray {
 
     static int getSmallest(int[] x){
 
         int small = Integer.MAX_VALUE;
-        
+
         for(int i = 0;i<x.length;i++){
 
             if ((x[i]<small)) {
@@ -16,5 +18,38 @@ public class MinArray {
             return small;
         }
     }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of an Arrays  : ");
+
+        int n = sc.nextInt();
+
+        int[] ar = new int[n];
+
+        System.out.print ("Enter"+n+"Values");
+
+        for(int i =0;i<ar.length;i++){
+
+            ar[i]  = sc.nextInt();
+        }
+
+        System.out.println("User entered Array Elements");
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.print("["+ar[i]+"] ");
+        }
+
+
+        int sm = getSmallest(ar);
+
+        System.out.println("Smallest element in array is :"+sm);
+
+    }
+
+   
     
 }
