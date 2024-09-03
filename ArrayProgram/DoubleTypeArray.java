740579c3e6fef0db5385f3a7a3a99be6652c0539
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 19:32:30 +0000

    initial

---
+++ ArrayProgram/DoubleTypeArray.java
@@ -0,0 +1,56 @@
+package ArrayProgram;
+import java.util.Scanner;
+public class DoubleTypeArray {
+    static int getbiggest(int[] x){
+
+        int big = Integer.MIN_VALUE;
+
+        for(int i = 0;i<x.length;i++){
+
+            if ((x[i]>big)) {
+                
+                big = x[i];
+            }
+
+            
+        }
+        return big;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of an Arrays  : ");
+
+        int n = sc.nextInt();
+
+        int[] ar = new int[n];
+
+        System.out.println ("Enter"+n+"Values");
+
+        for(int i =0;i<ar.length;i++){
+
+            ar[i]  = sc.nextInt();
+        }
+
+        System.out.println("User entered Array Elements");
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.print(ar[i]+"  ");
+        }
+        System.out.println();
+
+        System.out.println("-----------------------------------------------");
+
+        int sm = getbiggest(ar);
+
+        System.out.println("Smallest element in array is :"+sm);
+
+    }
+    
+}
+
+    
+
