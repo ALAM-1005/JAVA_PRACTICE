5f09cc10b80d27774d8458411f42e6143ab6ff91
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 14:49:51 +0000

    initial

--- ArrayProgram/MinArray.java
+++ ArrayProgram/MinArray.java
@@ -1,7 +1,31 @@
 package ArrayProgram;
 
 public class MinArray {
+ 
+    static int sumOfArray(int[] x )
+    
+    {
+        int sum = 0;
 
-    
-    
+        for(int i=0;i<=x.length;i++){
+            sum = sum+x[i];
+
+ 
+        }
+
+        return sum;
+     }
+
+
+     public static void main(String[]  args){
+
+        int[] ar = {23,45,56,63,39};
+
+        int sm = sumOfArray(ar);
+
+        System.out.println(sm);
+
+
+
+     }
 }
