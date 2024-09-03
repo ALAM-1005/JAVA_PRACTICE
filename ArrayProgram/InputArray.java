95d9fbdc64e0957956a7b161b0132130ac88b6ef
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 18:10:19 +0000

    user Array types

--- ArrayProgram/InputArray.java
+++ ArrayProgram/InputArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputArray {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the Size of an Arrays  : ");
@@ -17,6 +17,7 @@ public class InputArray {
         System.out.println("Enter"+n+"Values");
 
         for(int i =0;i<ar.length;i++){
+            
             ar[i]  = sc.nextInt();
         }
 
