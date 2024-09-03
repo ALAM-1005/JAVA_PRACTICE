e9c93791c5f76056ffe585126ab21df2ed1fc413
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 14:01:04 +0000

    initial

--- ArrayProgram/MainArray.java
+++ ArrayProgram/MainArray.java
@@ -1,10 +1,15 @@
 package JAVA_PRACTICE.ArrayProgram;
 
-class MainArray{
+/**
+ * MainArray
+ */
+public class MainArray {
+
+    
 
     public static void main(String[] args) {
 
-        int [] ar = {23,57,45,67,76};
+        int[] ar = {23,57,45,67,76};
 
         System.out.println(ar);
 
