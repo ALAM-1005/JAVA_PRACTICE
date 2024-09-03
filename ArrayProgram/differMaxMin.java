ed72abb047fee38505609afd5c71cdd07d0bdc43
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-03 19:18:07 +0000

    sacnner class imported

--- ArrayProgram/differMaxMin.java
+++ ArrayProgram/differMaxMin.java
@@ -1,6 +1,37 @@
 package ArrayProgram;
+import java.util.*;
 
 public class differMaxMin {
-    
+
+    static int differenceMinMax(int []x){
+
+        return MaxArray - MinArray;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of an Arrays  : ");
+
+        int n = sc.nextInt();
+
+        int[] ar = new int[n];
+
+        System.out.print ("Enter"+n+"Values");
+
+        for(int i =0;i<ar.length;i++){
+
+            ar[i]  = sc.nextInt();
+        }
+
+        System.out.println("User entered Array Elements");
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.print("["+ar[i]+"] ");
+        }
+    }
     
 }
