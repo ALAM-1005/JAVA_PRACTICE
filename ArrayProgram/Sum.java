d8de628cf7037dfed772a8c43ae760cc2d82ab88
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-04 14:11:32 +0000

    sum methods

--- ArrayProgram/Sum.java
+++ ArrayProgram/Sum.java
@@ -2,6 +2,16 @@ package ArrayProgram;
 
 public class Sum {
 
-    
+    static int sumOfArray(int[] x){
+
+        int sum =0;
+
+        for(int i =0;i<x.length;i++){
+
+            sum  = sum+x[i];
+
+        }
+        return sum;
+    }
     
 }
