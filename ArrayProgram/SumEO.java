40b4bc922b85624aa489e62821e59055262a4678
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-04 14:26:49 +0000

    initial

--- ArrayProgram/SumEO.java
+++ ArrayProgram/SumEO.java
@@ -2,6 +2,22 @@ package ArrayProgram;
 
 public class SumEO {
 
+    static int[] sumOfEO(int[] x){
+
+        int es=0,os = 0;
+
+        for(int i =0;i<x.length;i++){
+            if (x[i]%2 == 0)
+                    es = es+x[i];
+            else
+                    os = os+x[i];
+        }
+
+        int [] sum = {es,os}; 
+
+        return sum;
+    }
     
+
     
 }
