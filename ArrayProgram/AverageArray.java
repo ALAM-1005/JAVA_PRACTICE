7b291fc287d932ab590fbf0a409c220ac46520b8
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-04 13:55:47 +0000

    initial

--- ArrayProgram/AverageArray.java
+++ ArrayProgram/AverageArray.java
@@ -1,5 +1,30 @@
 package ArrayProgram;
 
 public class AverageArray {
+
+
+    static int sumOfArray(int[] x){
+
+        int sum =0;
+
+        for(int i =0;i<x.length;i++){
+
+            sum  = sum+x[i];
+
+
+        }
+        return sum;
+    }
+    static  double Average(int[] x){
+
+        double sum  = 0.0;
+
+        for(int i = 0;i<x.length;i++){
+
+            sum = sum+x[i];
+        }
+
+        return sum/x.length;
+    }
     
 }
