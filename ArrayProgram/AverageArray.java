2f2407e4b20a20fa294fa95759a774ae22dfa420
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-04 13:57:41 +0000

    initial

--- ArrayProgram/AverageArray.java
+++ ArrayProgram/AverageArray.java
@@ -1,5 +1,7 @@
 package ArrayProgram;
 
+import java.util.Scanner;
+
 public class AverageArray {
 
 
@@ -26,5 +28,38 @@ public class AverageArray {
 
         return sum/x.length;
     }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of an Arrays  : ");
+
+        int n = sc.nextInt();
+
+        int[] ar = new int[n];
+
+        System.out.print ("Enter"+n+"Values");
+
+        for(int i =0;i<ar.length;i++){
+
+            ar[i]  = sc.nextInt();
+        }
+
+        System.out.println("User entered Array Elements");
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.print("["+ar[i]+"] ");
+        }
+        System.out.println();
+
+        System.out.println("-----------------------------------------------");
+
+        double diff = Average(ar);
+
+        System.out.println("difference  element in array is :"+diff);
     
+    }
+
 }
