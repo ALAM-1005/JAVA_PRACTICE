f1bd19547a330dc72fdb83a97f16b3f8cc13079b
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-09 03:33:37 +0000

    arrays delecration in methods

--- ArrayProgram/MergeArray.java
+++ ArrayProgram/MergeArray.java
@@ -2,7 +2,7 @@ package ArrayProgram;
 
 public class MergeArray {
 
-    static int merge(int[]x,int[]y){
+    static int[] merge(int[]x,int[]y){
         int[] z = new int [x.length + y.length];
 
         for(int i = 0;i<x.length;i++){
