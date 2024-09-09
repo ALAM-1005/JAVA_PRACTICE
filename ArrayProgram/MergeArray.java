bc6be978a9096649c5d5d8ef6a92fe8b5cb27408
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-09 03:23:07 +0000

    Array declaration and initialization

--- ArrayProgram/MergeArray.java
+++ ArrayProgram/MergeArray.java
@@ -1,5 +1,11 @@
 package ArrayProgram;
 
 public class MergeArray {
+
+    public static void main(String[] args) {
+        int[] a = {25,56,98,45,12,54};
+
+        int [] b = {78,10,25};
+    }
     
 }
