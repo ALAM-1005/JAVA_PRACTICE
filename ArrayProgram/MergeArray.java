4ed30e2fb924339963b8e2ffe1b3c24eb5a97eb3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-09 03:26:05 +0000

    initial

--- ArrayProgram/MergeArray.java
+++ ArrayProgram/MergeArray.java
@@ -2,10 +2,22 @@ package ArrayProgram;
 
 public class MergeArray {
 
+    static int merge(int[]x,int[]y){
+        return z;
+    }
+
     public static void main(String[] args) {
         int[] a = {25,56,98,45,12,54};
 
         int [] b = {78,10,25};
+
+        int[] c = merge(a,b);
+
+        for(int i=0;i<c.length;i++){
+
+
+            System.out.println(c[i]+" ");
+        }
     }
     
 }
