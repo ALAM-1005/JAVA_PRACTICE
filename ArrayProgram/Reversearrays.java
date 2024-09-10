c997937e96114f6631374c243596f8346cb8887a
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-10 14:20:51 +0000

    arrays declaration

--- ArrayProgram/Reversearrays.java
+++ ArrayProgram/Reversearrays.java
@@ -1,5 +1,17 @@
 package ArrayProgram;
 
 public class Reversearrays {
+
+    public static void main(String[] args) {
+        
+
+        int[]  ar = {12,25,56,48,79};
+        reverseArr(ar);
+
+        for(int i = 0;i<ar.length;i++){
+
+            System.out.println(a[i]+ " ");
+        }
+    }
     
 }
