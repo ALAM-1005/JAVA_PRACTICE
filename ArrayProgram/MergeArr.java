498a30f0908bba99ca37976431623f08a3a9b1cb
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-10 17:13:46 +0000

    initial

--- ArrayProgram/MergeArr.java
+++ ArrayProgram/MergeArr.java
@@ -4,6 +4,11 @@ public class MergeArr {
 
     public static void main(String[] args) {
         
+
+        int[] a = {25,35,56,47,87,98,65,41};
+        int[]  b ={22,32,45,74,10};
+
+        
     }
     
 }
