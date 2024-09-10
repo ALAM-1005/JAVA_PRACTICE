73093a6007a7eb5ee6e818b4e2c9e4bdf7ed40e3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-10 17:55:05 +0000

    initial

--- ArrayProgram/ZigZagArr.java
+++ ArrayProgram/ZigZagArr.java
@@ -8,17 +8,19 @@ public class ZigZagArr {
 
         
         int i =0, j=0, k=0;
+        
         while (i<a.length  && j<b.length) {
             
             c[k++] = a[i++];
 
             c[k++] = b[j++];
         }
+
         while(i<a.length){
 
             c[k++] = a[i++];
         }
-        
+
         while(i<b.length){
 
             c[k++] = b[j++];
