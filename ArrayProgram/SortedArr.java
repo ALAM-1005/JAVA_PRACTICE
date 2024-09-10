ce5883426097fb7eb966138c0385fb00a1d33480
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-10 18:16:48 +0000

    initial

--- ArrayProgram/SortedArr.java
+++ ArrayProgram/SortedArr.java
@@ -1,5 +1,52 @@
 package ArrayProgram;
 
 public class SortedArr {
+    static int[] zigZag(int[] a,int[] b){
+
+
+
+        int[] c = new int[a.length + b.length ];
+
+        
+        int i =0, j=0, k=0;
+
+        while (i<a.length  && j<b.length) {
+            
+            c[k++] = a[i++];
+
+            c[k++] = b[j++];
+        }
+
+        while(i<a.length){
+
+            c[k++] = a[i++];
+        }
+
+        while(i<b.length){
+
+            c[k++] = b[j++];
+        }
+
+    return c;
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        int[] a = {25,35,47,56,75,95};
+
+        int[]  b ={22,32,45,74};
+
+        int[] c = mergeSort(a,b);  // every arrays elements one by one strore
+
+        for(int i =0;i<c.length;i++){
+
+            System.out.print(c[i]+" ");
+        }
+     System.out.println();
+
+
+    }
     
 }
