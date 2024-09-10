e8fac0e3aedfaa84ab0adb884bc95512ef74a9ac
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-10 17:48:02 +0000

    variable declaration

--- ArrayProgram/ZigZagArr.java
+++ ArrayProgram/ZigZagArr.java
@@ -6,10 +6,12 @@ public class ZigZagArr {
 
         int[] c = new int[a.length + b.length ];
 
+        int i,j,k = 0;
         while (i<a.length  && j<b.length) {
 
             c[k++] a[i++];
-            
+
+            c[k++] = b[j++];
         }
 
 
