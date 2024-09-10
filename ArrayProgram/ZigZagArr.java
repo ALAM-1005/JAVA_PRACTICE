b638e741e5f01232d5012a3438356b67175c20d8
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-10 17:46:56 +0000

    method declaration

--- ArrayProgram/ZigZagArr.java
+++ ArrayProgram/ZigZagArr.java
@@ -7,7 +7,8 @@ public class ZigZagArr {
         int[] c = new int[a.length + b.length ];
 
         while (i<a.length  && j<b.length) {
-            
+
+            c[k++] a[i++];
             
         }
 
