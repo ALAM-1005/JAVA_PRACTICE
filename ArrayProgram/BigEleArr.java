fae5753fd3320ec0a435d26c77e4f8d5908d5599
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-11 17:47:31 +0000

    initial

--- ArrayProgram/BigEleArr.java
+++ ArrayProgram/BigEleArr.java
@@ -9,13 +9,14 @@ public class BigEleArr {
             int count = 0;
 
 
-            for(int j =0;j<x.length;j++){
+            for(int j =0; j<x.length; j++){
 
                 if(x[j] >x[i]){
 
                     count++;
                 }
                 if(count == n-1)
+
                 return x[i];
             }
         }
@@ -29,7 +30,7 @@ public class BigEleArr {
     public static void main(String[] args) {
 
         int[] ar = {25,36,45,78,96};
-        
+
         int bg = nthBiggest(ar, 4);
 
         System.out.println("4th Biggest elements :"+bg);
