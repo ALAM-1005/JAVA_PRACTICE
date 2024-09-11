23cf69c70f2ef264f487c8e46f8dcb5a1d25c4d2
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-11 02:14:35 +0000

    initial

--- ArrayProgram/RevSortedArr.java
+++ ArrayProgram/RevSortedArr.java
@@ -1,5 +1,51 @@
 package ArrayProgram;
 
 public class RevSortedArr {
+
+    static int[] revMergeSort(int[] a,int[] b){
+
+        int[] c = new int[a.length + b.length ];
+
+        int i =a.length-1, j=b.length -1, k=0;
+
+        while (i>=0 && j>=0) {
+
+            if(a[i] > b[j])
+                c[k++] = a[i--];
+            else
+                c[k++] = b[j--];
+        }
+
+        while(i<a.length){
+
+            c[k++] = a[i--];
+        }
+
+        while(i<b.length){
+
+            c[k++] = b[j--];
+        }
+
+    return c;
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        int[] a = {25,35,47,56,75,95};
+
+        int[]  b ={22,32,45,74};
+
+        int[] c = revMergeSort(a,b);  // every arrays elements one by one strore
+
+        for(int i =0;i<c.length;i++){
+
+            System.out.print(c[i]+" ");
+        }
+     System.out.println();
+
+
+    }
     
 }
