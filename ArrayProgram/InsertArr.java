f0c91a52ddaced933348999b297a536eb177b40d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-11 02:49:16 +0000

    initial

--- ArrayProgram/InsertArr.java
+++ ArrayProgram/InsertArr.java
@@ -5,10 +5,11 @@ public class InsertArr {
     static int[] insertEle(int[] x , int ele,int in){
 
         if(in<0 || in>x.length)
+        
             return null;
 
         int [] y = new int[x.length + 1];
-        
+
          y[in] = ele;
 
          for(int i= 0;i<x.length;i++){
