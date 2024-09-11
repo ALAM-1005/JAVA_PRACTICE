932747032189bf5336388adbf431f295d029a911
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-11 18:24:51 +0000

    initial

--- ArrayProgram/RepeatEle.java
+++ ArrayProgram/RepeatEle.java
@@ -1,5 +1,26 @@
 package ArrayProgram;
 
 public class RepeatEle {
+
+    static void printFrequence(int[] x){
+
+        boolean [] rs = new boolean[x.length];
+        
+        for(int i =0;i<x.length;i++){
+
+            if(rs[i] == false){
+
+                int count =1;
+
+                for(int j =i+1;j<x.length;j++){
+
+                    if(x[i] == x[j])
+                    {
+                        count++;
+                    }
+                }
+            } 
+        }
+    }
     
 }
