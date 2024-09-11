0cb1ec125ef2d99ee72986436885d4cb4a8391e3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-11 13:05:00 +0000

    initial

--- ArrayProgram/BigEleArr.java
+++ ArrayProgram/BigEleArr.java
@@ -2,6 +2,30 @@ package ArrayProgram;
 
 public class BigEleArr {
 
-    static
+    static int nthBiggest(int[] x,int n){
+
+        for(int i =0;i<x.length;i++){
+
+            int count = 0;
+            for(int j =0;j<x.length;j++){
+
+                if(x[i]>x[j]{
+                    count++;
+                }
+                if(count == n-1)
+                return x[i];
+            }
+        }
+        System.out.println("No Biggest Number");
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {25,36,45,78,96};
+        int bg = nthBiggest(ar, 4);
+
+        System.out.println("4th Biggest elements :"+bg);
+     }
     
 }
