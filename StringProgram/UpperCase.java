fb172b232e16e863a7aaf05bcc1605e6543de760
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-12 20:13:57 +0000

    initial

--- StringProgram/UpperCase.java
+++ StringProgram/UpperCase.java
@@ -5,31 +5,25 @@ public class UpperCase {
     public static void main(String[] args) {
 
 
-        String str = "1020";
-
+        String str = "Java";
+        char[] ch = str.toCharArray();
          // string to char
-        int sum = 0;
         for(int i = 0;i<str.length();i++){
 
-            char ch = str.intAt(i); 
-System.out.println(ch+sum);
 
-            if(ch>=0 && ch<=9){
-                sum = (int)(ch+sum);
-            }
-                
+               
 
-            // if(ch[i]>='a' && ch[i]<='z'){
+             if(ch[i]>='a' && ch[i]<='z'){
 
-            //     ch[i] = (char)(ch[i]-32);  // convert into the Upper case letter
-            // }
+                ch[i] = (char)(ch[i]-32);  // convert into the Upper case letter
+             }
 
 
         }
-        // str = new String(ch);  // Assign into string and pass the Character in string method
+         str = new String(ch);  // Assign into string and pass the Character in string method
 
 
-        System.out.println(sum);
+        System.out.println(str);
     }
     
 }
