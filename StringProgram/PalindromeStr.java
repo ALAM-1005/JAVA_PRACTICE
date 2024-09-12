fcc7fb7e98f39cde55a188ae2a03d4d0e06fd016
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-12 20:43:14 +0000

    intital

--- StringProgram/PalindromeStr.java
+++ StringProgram/PalindromeStr.java
@@ -1,5 +1,17 @@
 package StringProgram;
 
 public class PalindromeStr {
+static String Palindrome(String[] str)
+    public static void main(String[] args) {
+        String s = "Malayalam";
+
+        char[] ch = s.toCharArray();
+
+        for(int i =ch.length -1;i>=0;i--){
+
+            System.out.print(ch[i]+" ");
+
+        }
+    }
     
 }
