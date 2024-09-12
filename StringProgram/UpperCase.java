af65491e854a03322aeaba788a20753f2463c6d6
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-12 17:46:37 +0000

    intital

--- StringProgram/UpperCase.java
+++ StringProgram/UpperCase.java
@@ -3,7 +3,20 @@ package StringProgram;
 public class UpperCase {
 
     public static void main(String[] args) {
-        
+        String str = "Java";
+        char[] ch = str.toCharArray();
+
+        for(int i = 0;i<ch.length;i++){
+
+            if(ch[i]>='a' && ch[i]<='z'){
+
+                ch[i] = (char)(ch[i]-32);
+            }
+
+
+        }
+        str = new String(ch);
+        System.out.println(str);
     }
     
 }
