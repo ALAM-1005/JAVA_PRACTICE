6061e64f3ec33f25cd256bb737eb46124ff0e993
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-12 18:34:04 +0000

    initial

--- StringProgram/VovelConst.java
+++ StringProgram/VovelConst.java
@@ -1,5 +1,30 @@
 package StringProgram;
 
 public class VovelConst {
+
+    public static void main(String[] args) {
+
+
+        String str = "Jave";
+
+        char[] ch = str.toCharArray();  // string to char
+
+        int v = 0,c =0;
+        for(int i = 0;i<ch.length;i++){
+
+            if(ch[i] =='a'|| ch[i]=='e' ||ch[i]=='i' ||  ch[i]=='o' || ch[i]=='u'){
+
+               v++;
+            }
+            else
+            c++;
+
+
+        }
+        str = new String(ch);  // Assign into string and pass the Character in string method
+
+
+        System.out.println(str);
+    }
     
 }
