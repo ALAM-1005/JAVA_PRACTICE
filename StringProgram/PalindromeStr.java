d8f7207d2eb8dd6be22ec051ec4ba79be9065e9f
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-13 12:54:11 +0000

    initial

--- StringProgram/PalindromeStr.java
+++ StringProgram/PalindromeStr.java
@@ -1,18 +1,39 @@
 package StringProgram;
 
 public class PalindromeStr {
-static boolean isPalindrome(String str)
+static boolean isPalindrome(String str){
+
+
+    str = str.toLowerCase();
+
+    int i = 0,j =str.length()-1;
+    while(i<j){
+
+        if(str.charAt(i) != str.charAt(j)){
+
+            return false;
+        }
+
+        i++;
+        j++;
+    }
+    return true;
+}
 
     public static void main(String[] args) {
         String s = "Malayalam";
 
-        char[] ch = s.toCharArray();
+        // char[] ch = s.toCharArray();
 
-        for(int i =ch.length -1;i>=0;i--){
+        // for(int i =ch.length -1;i>=0;i--){
 
-            System.out.print(ch[i]+" ");
+        //     System.out.print(ch[i]+" ");
 
-        }
+        // }
+
+       System.out.println(isPalindrome(s)); 
+
+
     }
     
 }
