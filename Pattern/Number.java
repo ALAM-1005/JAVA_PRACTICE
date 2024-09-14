b471ca13b92878197dd94562e7fb2bceb858ad20
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:36:02 +0000

    initial

--- Pattern/Number.java
+++ Pattern/Number.java
@@ -16,7 +16,7 @@ public class Number {
 
             for(int j =1;j<=n;j++){
 
-                System.out.print( +" ");
+                System.out.print(k +" ");
             }
             System.out.println();
         }
