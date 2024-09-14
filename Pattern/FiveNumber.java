b699a04d34ef44d2108a9a59742aee23f640de29
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:25:43 +0000

    initial

--- Pattern/FiveNumber.java
+++ Pattern/FiveNumber.java
@@ -1,5 +1,29 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class FiveNumber {
     
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter  the Number");
+
+        int n = sc.nextInt();
+
+        for(int i = 1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+
+                System.out.print("* "+" ");
+            }
+            System.out.println();
+        }
+
+
+       
+    }
 }
