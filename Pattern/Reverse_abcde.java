0023400ce501c3c9556af28d6321501a18357e1c
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 18:07:08 +0000

    initial

--- Pattern/Reverse_abcde.java
+++ Pattern/Reverse_abcde.java
@@ -1,5 +1,12 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class Reverse_abcde {
     
+
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+    }
 }
