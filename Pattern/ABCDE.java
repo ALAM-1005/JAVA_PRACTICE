eeed11519a0cfef02b8fe98c2ec23ceb7270f797
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:30:46 +0000

    initial

--- Pattern/ABCDE.java
+++ Pattern/ABCDE.java
@@ -7,9 +7,11 @@ public class ABCDE {
 
      public static void main(String[] args) {
 
-        Scanner sc  = new Scanner(System.in);
 
         
+        Scanner sc  = new Scanner(System.in);
+
+
 
         System.out.println("Enter  the Number");
 
