e680c4204c1c7356c3fe113bc1a0496f99632fe2
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:50:41 +0000

    initial

--- Pattern/AtoZ.java
+++ Pattern/AtoZ.java
@@ -1,5 +1,29 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class AtoZ {
+
+     public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter  the Number");
+
+        int n = sc.nextInt();
+
+        int k =1;
+
+        for(int i = 1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+                // System.out.println(k+" ");
+                // System.out.printf("%3d ",k);
+                System.out.printf("%2d ", k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
     
 }
