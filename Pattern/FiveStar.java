843af58fd14a12b7e305967ccda457322ce16544
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:21:48 +0000

    initial

--- Pattern/FiveStar.java
+++ Pattern/FiveStar.java
@@ -1,5 +1,23 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class FiveStar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+
+                System.out.print("*"+" ");
+            }
+        }
+
+        System.out.println();
+    }
     
 }
