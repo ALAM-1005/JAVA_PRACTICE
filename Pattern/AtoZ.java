c4c3586c134fd73257ce45b47eda52d386a0b1cf
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:57:03 +0000

    initial

--- Pattern/AtoZ.java
+++ Pattern/AtoZ.java
@@ -17,10 +17,9 @@ public class AtoZ {
         for(int i = 1;i<=n;i++){
 
             for(int j =1;j<=n;j++){
-
-
+                
                  System.out.print((char)(k+64)+" ");
-                 
+
                 k++;
 
                 if (k == 27) {
