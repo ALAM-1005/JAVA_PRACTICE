e1ee17a2cd3a4ce94cac15140107b0aa6bfd7af9
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:22:06 +0000

    initial

--- Pattern/FiveStar.java
+++ Pattern/FiveStar.java
@@ -10,13 +10,14 @@ public class FiveStar {
         int n = sc.nextInt();
 
         for(int i = 1;i<=n;i++){
+            
 
             for(int j =1;j<=n;j++){
 
                 System.out.print("*"+" ");
             }
         }
-        
+
 
         System.out.println();
     }
