926e5f5332ff776d4d491574f8b9c080714bebe1
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 18:22:09 +0000

    initial

--- Pattern/ReverseNumber.java
+++ Pattern/ReverseNumber.java
@@ -1,7 +1,28 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
 
-    
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+
+        int n  = sc.nextInt();
+
+        for(int i =0;i<=n;i++){
+
+            for(int j =n-1;j>=0;j--) {
+
+                System.out.print((char)(j+97)+" ");
+
+        }
+        System.out.println();
+
+
+    }
+}
     
 }
