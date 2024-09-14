997ecad8fd17afa67596253547428077071593cb
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:34:50 +0000

    initial

--- Pattern/Number.java
+++ Pattern/Number.java
@@ -1,5 +1,25 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class Number {
+
+    ublic static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter  the Number");
+
+        int n = sc.nextInt();
+
+        for(int i = 1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+
+                System.out.print((char)(j+64) +" ");
+            }
+            System.out.println();
+        }
+    }
     
 }
