90df2e3f18cf3dfba0a19ac050f89843c73d4840
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 17:29:57 +0000

    initial

--- Pattern/ABCDE.java
+++ Pattern/ABCDE.java
@@ -1,5 +1,26 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class ABCDE {
     
+
+     public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter  the Number");
+
+        int n = sc.nextInt();
+
+
+        for(int i = 1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+
+
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
 }
