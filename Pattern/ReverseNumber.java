6649a1439f1c64ca37a50d41163be0ede105775b
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 18:24:23 +0000

    initial

--- Pattern/ReverseNumber.java
+++ Pattern/ReverseNumber.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ReverseNumber {
 
+    
      public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -19,7 +20,7 @@ public class ReverseNumber {
             System.out.print(j+" ");
 
         }
-        
+
         System.out.println();
 
 
