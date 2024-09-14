f3de975c5bb07b4ff9991ec6e3f1ea45df253d96
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-14 18:08:09 +0000

    initial

--- Pattern/Reverse_abcde.java
+++ Pattern/Reverse_abcde.java
@@ -8,9 +8,9 @@ public class Reverse_abcde {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-
+System.out.println("Enter the number");
         int n  = sc.nextInt();
 
-        
+
     }
 }
