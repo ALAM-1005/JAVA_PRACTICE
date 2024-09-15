fea58acd5a4eb314e8bd71b75bbc545f29ef18fa
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 13:41:06 +0000

    initial

--- Pattern/NumberPramid.java
+++ Pattern/NumberPramid.java
@@ -1,5 +1,11 @@
 package Pattern;
-
+import java.util.Scanner;
 public class NumberPramid {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+    }
     
 }
