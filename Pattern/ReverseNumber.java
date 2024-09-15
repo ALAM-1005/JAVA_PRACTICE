f7742107fce2b7383a3c9aca343ffabad16e22f3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 12:20:57 +0000

    initial

--- Pattern/ReverseNumber.java
+++ Pattern/ReverseNumber.java
@@ -13,7 +13,9 @@ public class ReverseNumber {
         int n  = sc.nextInt();
 
         for(int i =0;i<=n;i++){
+
             for(int j =n-1;j>=0;j--) {
+                
             System.out.print(j+" ");
 
         }
