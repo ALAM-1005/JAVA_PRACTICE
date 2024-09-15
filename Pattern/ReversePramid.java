c34142e7330fd868226898879af5ca57b89898e3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 13:30:54 +0000

    initial

--- Pattern/ReversePramid.java
+++ Pattern/ReversePramid.java
@@ -1,5 +1,11 @@
 package Pattern;
-
+import java.util.*;
 public class ReversePramid {
+
+    Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter  the Number");
+
+        int n = sc.nextInt();
     
 }
