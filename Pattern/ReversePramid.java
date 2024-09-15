9a0aa197bf1f07532ea0cb03747b4f103a5afc59
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 13:31:26 +0000

    initial

--- Pattern/ReversePramid.java
+++ Pattern/ReversePramid.java
@@ -1,11 +1,23 @@
 package Pattern;
 import java.util.*;
 public class ReversePramid {
+    public static void main(String[] args) {
 
-    Scanner sc  = new Scanner(System.in);
 
-        System.out.println("Enter  the Number");
+        Scanner sc  = new Scanner(System.in);
 
-        int n = sc.nextInt();
-    
+    System.out.println("Enter  the Number");
+
+    int n = sc.nextInt();
+
+        for(int line =1;line<=n;line++){
+
+            for(int star =1;star<=line;star++){
+
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+}
 }
