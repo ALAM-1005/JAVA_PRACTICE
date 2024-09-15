0dbf671b7ceb4ffcaaf6b8e1b4393b48c63830ed
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 13:35:53 +0000

    initial

--- Pattern/ReversePramid.java
+++ Pattern/ReversePramid.java
@@ -12,8 +12,9 @@ public class ReversePramid {
 
         for(int line =1;line<=n;line++){
 
-            
+
             for(int star =1;star<=n-line+1;star++){
+                
 
                 System.out.print("*"+" ");
             }
