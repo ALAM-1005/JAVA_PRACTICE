14f8fd19e0667e7d87a73c27d24aead9fa55927a
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 12:50:30 +0000

    initial

--- Pattern/HalfPramid.java
+++ Pattern/HalfPramid.java
@@ -1,5 +1,25 @@
 package Pattern;
-
+import java.util.*;
 public class HalfPramid {
+
+        public static void main(String[] args) {
+
+
+            Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter  the Number");
+
+        int n = sc.nextInt();
+
+            for(int line =1;line<=n;line++){
+
+                for(int star =1;star<=line;star++){
+
+                    System.out.print("*"+" ");
+                }
+                System.out.println();
+            }
+    
+}
     
 }
