b4c7e96e0ae384e238289da3e11dace2a5d16406
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-15 13:47:44 +0000

    initial

--- Pattern/NumberPramid.java
+++ Pattern/NumberPramid.java
@@ -8,9 +8,9 @@ public class NumberPramid {
         System.out.println("Enter the Number");
         int n = sc.nextInt();
 
-        for(int i = 1;i<=n;i++){
+        for(int i =1;i<=n;i++){
 
-            for(int j =1;j<=n;j++){
+            for(int j = 1;j<=n;j++){
 
                 System.out.print(j);
             }
