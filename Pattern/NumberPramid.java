93db40acd0616cddff7464857bb4cd47197a08ad
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-16 14:00:38 +0000

    initial

--- Pattern/NumberPramid.java
+++ Pattern/NumberPramid.java
@@ -5,6 +5,7 @@ public class NumberPramid {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Enter the Number");
         int n = sc.nextInt();
 
