908cc0422a0fe2ade866e955b474549a75f20feb
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-17 17:09:29 +0000

    initial

--- StringProgram/InitSmall.java
+++ StringProgram/InitSmall.java
@@ -1,5 +1,45 @@
 package StringProgram;
 
+import java.util.Scanner;
+
 public class InitSmall {
     
+
+
+    static String initSmall(String str){
+
+        char[] ch = str.toCharArray();
+
+        for(int i =0;i<ch.length;i++){
+
+            if(i ==0 && ch[i] != ' '  || ch[i]!= ' ' && ch[i-1] == ' '){
+
+                if(ch[i]>= 'a' && ch[i]<='z')
+                ch[i] = (char)(ch[i] - 32);
+                else{
+
+                    if(ch[i]>='A'  &&  ch[i]<= 'Z')
+                    ch[i] = (char)(ch[i] + 32);
+                }
+            }
+
+            return new String(ch);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String");
+
+        String str = sc.nextLine();
+        
+
+        String rs = initSmall(str);
+
+       System.out.println(rs);
+    }
 }
