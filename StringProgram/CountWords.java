b9b1fc5417c27433b0a50c4f4338e51efcab746e
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 19:33:50 +0000

    initial

--- StringProgram/CountWords.java
+++ StringProgram/CountWords.java
@@ -1,5 +1,32 @@
 package StringProgram;
-
+import java.util.*;
 public class CountWords {
-    
+    static int countWords(String str){
+
+        int count =0;
+
+        char[] ch = str.toCharArray();
+
+        for(int i =0;i<ch.length;i++){
+            if(i==0&&ch[i]!=' '||ch[i]!=' '&&ch[i-1]==' '){
+                if((ch[i] == 'a'||ch[i] == 'e'||ch[i] == 'i'||ch[i] == 'o'||ch[i] == 'u')||(ch[i] == 'A'||ch[i] == 'E'||ch[i] == 'I'||ch[i] == 'O'||ch[i] == 'U'))
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String");
+
+        String str = sc.nextLine();
+        int  count = countWords(str);
+
+       System.out.println(count);
+    }
 }
