7baa1d1445ed5e707b8330646258a91286e26e16
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 18:41:45 +0000

    initial

--- StringProgram/InitSmall.java
+++ StringProgram/InitSmall.java
@@ -13,7 +13,6 @@ public class InitSmall {
         for(int i=0;i <ch.length;i++)
         
         {
-
             if(i==0&&ch[i]!=' '||ch[i]!=' '&&ch[i-1]==' '){
 
                 if(ch[i] >= 'a' && ch[i]<='z'){
--- StringProgram/LastCap.java
+++ StringProgram/LastCap.java
@@ -9,23 +9,25 @@ public class LastCap {
 
         char[] ch = str.toCharArray();
 
-        for(int i=0;i <ch.length;i++){
+        for(int i=0; i <ch.length; i++)
+        {
 
-            if(i == ch.length-1 && ch[i] != ' '|| ch[i] != ' '&&ch[i+1] == ' '){
+            if(i==ch.length-1&&ch[i]!=' '||ch[i]!=' '&&ch[i+1]==' ')
+            {
 
-                if(ch[i]>= 'a' && ch[i]<='z'){
-                    ch[i] = (char)(ch[i] - 32);
+                if(ch[i]>='a'&&ch[i]<='z'){
+                    ch[i]=(char)(ch[i]-32);
                 }
-                else{
-
-                    if(ch[i]>='A'&&ch[i]<= 'Z')
-
-                    ch[i] = (char)(ch[i]); // keep it same 
-                }
+                
             }
+            else{
 
+                if(ch[i]>='A'&&ch[i]<='Z')
+                    ch[i]=(char)(ch[i]+32); // keep it same 
+            }
+           
         }
-        return new String(ch);
+        return new String(ch); 
     }
      public static void main(String[] args) {
         
