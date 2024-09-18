897b52f449f45c194d5b013f014fdf0fe7717e40
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 14:44:53 +0000

    initial

--- StringProgram/SwapFL.java
+++ StringProgram/SwapFL.java
@@ -1,5 +1,44 @@
 package StringProgram;
 
+import java.util.Scanner;
+
 public class SwapFL {
+
+    static String swapFL(String str){
+
+        char[] ch =str.toCharArray();
+        int f =0;
+
+        for(int i=0;i<ch.length;i++){
+
+            if(i ==0 && ch[i]!=' '||ch[i] != ' '&&ch[i-1]==' '){
+
+                f = i;
+            }
+
+            else(i == ch.length-1 && ch[i]!=' ' || ch[i] != ' ' && ch[i+1] == ' ')
+            {
+
+                char t = ch[f];
+                ch[f] = ch[i];
+                ch[i] = t;
+            }
+        }
+
+        return new String(ch);
+    }
+
+     public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String");
+
+        String str = sc.nextLine();
+         str = swapFL(str);
+
+       System.out.println(str);
+    }
     
 }
