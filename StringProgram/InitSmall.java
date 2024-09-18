ee8416e843affb4ab16ab3e6c5aec8aea123ea2b
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 00:40:38 +0000

    initial

--- StringProgram/InitSmall.java
+++ StringProgram/InitSmall.java
@@ -12,7 +12,7 @@ public class InitSmall {
 
         for(int i =0;i<ch.length;i++){
 
-            if(i ==0 && ch[i] != ' '  || ch[i]!= ' ' && ch[i-1] == ' '){
+            if(i == 0 && ch[i] != ' '  || ch[i]!= ' ' && ch[i-1] == ' '){
 
                 if(ch[i]>= 'a' && ch[i]<='z')
                 ch[i] = (char)(ch[i] - 32);
