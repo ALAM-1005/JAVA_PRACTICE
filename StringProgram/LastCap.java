d1f82b81852ca84712855e174068a3b073214651
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 13:46:18 +0000

    initial

--- StringProgram/LastCap.java
+++ StringProgram/LastCap.java
@@ -2,4 +2,27 @@ package StringProgram;
 
 public class LastCap {
     
+
+    static String lastCapital(String str){
+
+        char[] ch = str.toCharArray();
+
+        for(int i=0;i <ch.length;i++){
+
+            if(i == ch.length-1 && ch[i] != ' '|| ch[i] != ' '&&ch[i-1] == ' '){
+
+                if(ch[i]>= 'a' && ch[i]<='z'){
+                    ch[i] = (char)(ch[i] - 32);
+                }
+                else{
+
+                    if(ch[i]>='A'&&ch[i]<= 'Z')
+
+                    ch[i] = (char)(ch[i] + 32);
+                }
+            }
+
+        }
+        return new String(ch);
+    }
 }
