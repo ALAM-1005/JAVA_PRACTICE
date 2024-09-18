05a3b3c890a37a81d98b972f06ba45895f30587b
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 18:58:29 +0000

    initial

--- StringProgram/FLCaps.java
+++ StringProgram/FLCaps.java
@@ -10,6 +10,7 @@ public class FLCaps {
 
         for(int i=0;i <ch.length;i++)
         
+        
         {
             if((i==0&&ch[i]!=' '||ch[i]!=' '&&ch[i-1]==' ')||(i == ch.length-1 && ch[i]!=' ' || ch[i] != ' ' && ch[i+1] == ' ')){
 
