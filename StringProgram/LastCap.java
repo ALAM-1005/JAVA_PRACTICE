eb8c500304de48fce9716a42974f17459026f867
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-18 17:54:20 +0000

    initial

--- StringProgram/LastCap.java
+++ StringProgram/LastCap.java
@@ -20,7 +20,7 @@ public class LastCap {
 
                     if(ch[i]>='A'&&ch[i]<= 'Z')
 
-                    ch[i] = (char)(ch[i]); // giving right answer 
+                    ch[i] = (char)(ch[i]); // keep it same 
                 }
             }
 
