09879e53d754034d1c4472f35082dab82e7e4e3d
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-24 17:34:21 +0000

    initial

--- _2DArray/RowWiserev.java
+++ _2DArray/RowWiserev.java
@@ -1,5 +1,46 @@
 package _2DArray;
 
+import java.util.Arrays;
+
 public class RowWiserev {
     
+
+    void rowWiseRev(int [][] mat){
+
+        for(int i =0;i<mat.length;i++){
+
+            int l =0, h = mat[i].length -1;
+
+            while (l<h) {
+
+                int temp = mat[i][l];
+                mat[i][l] = mat[i][h];
+                mat[i][h] = temp;
+
+                l++;
+                h--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Matrix mt = new Matrix();
+        
+        System.out.println("read first Matrix");
+        
+        int[][]a=mt.readMat();
+        
+        System.out.println("user entered matrix"); 
+        mt.dispMat(a);
+        
+        int[] rs = rowWiseRev(a);
+        
+        for (int i=0; i<rs.length; i++)
+        
+        {
+        
+        System.out.println(i+1+" RowWise smallest element is: " +rs[i]);
+        }
+    }
 }
