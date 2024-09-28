748ae121f26cd2bfb114d41d8910ac95ad1cadd6
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-28 10:16:00 +0000

    initial

--- _2DArray/RowWiserev.java
+++ _2DArray/RowWiserev.java
@@ -21,12 +21,15 @@ public class RowWiserev {
             //System.out.println(mat);
         }
     }
-public static void main(String[] args) {
+        public static void main(String[] args) {
 
-    
-    int[][] ar ={ {1,2,3},
-                {4, 5, 6},
-                {7, 8, 9}};
+
+            int[][] ar ={ 
+                
+                        {1,2,3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                    };
 
                 rowWiseRev(ar);
 
