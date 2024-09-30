796bd44c10818e1ecf044940c363cba718810d71
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-30 17:33:34 +0000

    initial

--- ArrayProgram/SumE.java
+++ ArrayProgram/SumE.java
@@ -2,4 +2,28 @@ package ArrayProgram;
 
 public class SumE {
     
+
+    static int sum(int[] x){
+
+        int sum  =0;
+
+        for(int i =0; i<x.length;i++){
+
+            if(i%2 == 0){  
+
+                sum = sum + x[i];
+            }
+        }
+        return sum;
+    } 
+
+
+    public static void main(String[] args) {
+        
+        int[] ar = { 0,1 ,3,4,5,6};   
+
+        int sm = sum(ar);
+
+       System.out.println(sm);
+    }
 }
